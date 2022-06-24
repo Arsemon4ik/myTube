8eca938cf728fd416136ee4e4fb515942e6ec197
Arsemon4ik <misterarsemon4ik@gmail.com>
2022-06-24 14:36:18 +0300

    some edit wuth AWS

--- backend/myTube/sign/forms.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.forms import UserCreationForm
-
-from .models import User
-
-
-class UserForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'phone', 'avatar', 'password1', 'password2']
--- backend/myTube/tube/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Comment, Subscribe, Video, Author
-
-# Register your models here.
-admin.site.register(Comment)
-admin.site.register(Subscribe)
-admin.site.register(Video)
-admin.site.register(Author)
